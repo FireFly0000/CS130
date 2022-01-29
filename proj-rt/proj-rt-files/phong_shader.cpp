df49f15eb9146abb4d9d7ef766a4a9636c08407a
FireFly0000 <wktfirefly283@gmail.com>
2022-01-29 01:09:52 -0800

    finishing ambient component in phong shading Shade_Surface

--- proj-rt/proj-rt-files/phong_shader.cpp
+++ proj-rt/proj-rt-files/phong_shader.cpp
@@ -9,6 +9,17 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
     vec3 color;
-    TODO; //determine the color
+    //TODO; //determine the color
+    
+    //calculating ambient color component Ia = Ra * La ====================
+    //Ra = color of the object
+    //La = ambient light intensity
+
+    color = (color_ambient * world.ambient_color) * world.ambient_intensity;
+    
+    // ====================================================================
+    //calculating difuse and specular components ==========================
+    
+
     return color;
 }
